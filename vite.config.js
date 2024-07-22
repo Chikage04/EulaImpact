f4e36de09890f2f382a4292ab8cb139b13027fbb
chikage <lucas.benjebara9@gmail.com>
2024-07-23 01:29:12 +0200

    update port

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 8000 // Remplacer 3000 par le port souhaité
+	  }
 });
